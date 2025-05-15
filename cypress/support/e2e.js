a27e7ff9d6825ca29ba113c7146498b06fe52edb
Diego Pedroso <diegospedroso@gmail.com>
2025-05-15 13:42:45 -0300

    test: adjustments on automation

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,1 +1,6 @@
 import './login_commands';
+import './verifyItem_commands';
+import './findProduct_commands';
+import './exercises_commands';
+import './verifyCheckboxState_commands';
+import './verifyElementRelations_commands';
